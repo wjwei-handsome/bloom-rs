11928b79cf9877ae4127c4c34a0bff8d8d7b4770
wjwei-handsome <56633839+wjwei-handsome@users.noreply.github.com>
2023-06-02 14:02:43 +0800

    basic impl but just 1 hasher;calculate fp-rate for test

--- src/bf.rs
+++ src/bf.rs
@@ -1,8 +1,8 @@
 // 1. we need a bit array: bitvec
 
 use std::collections::hash_map::{DefaultHasher, RandomState};
-use std::hash::{BuildHasher, Hash};
-use std::os::fd::BorrowedFd;
+use std::hash::{BuildHasher, Hash, Hasher};
+use std::marker::PhantomData;
 use bitvec::prelude::*;
 
 // define a constant bit array size: 10240
@@ -14,38 +14,71 @@ type HasherArray = Box<[Box<dyn BuildHasher<Hasher = DefaultHasher>>]>;
 pub struct BloomFilter<T: Hash + ?Sized> {
     capacity: usize, // Bit array size
     hashers: HasherArray, // Hasher array
-    bit_array: BitVec // Bit array
+    bit_array: BitVec, // Bit array
+    // as compiler suggested, we can use PhantomData to make the type T used
+    _phantom: PhantomData<T>
 }
 
 // impl a default trait for BloomFilter
 
-impl Default for BloomFilter<T> {
+impl<T: Hash + ?Sized> Default for BloomFilter<T> {
     fn default() -> Self {
-        let hash_init_vec = vec![Box::new(RandomState::new())];
+        let hash_init_vec: Vec<Box<dyn BuildHasher<Hasher = DefaultHasher>>> = vec![Box::new(RandomState::new())];
         // different RandomState will generate different hashers
         let hash_array = HasherArray::from(hash_init_vec);
         BloomFilter {
             capacity: BIT_ARRAY_SIZE,
             hashers: hash_array,
-            bit_array: BitVec::repeat(false, BIT_ARRAY_SIZE)
+            bit_array: BitVec::repeat(false, BIT_ARRAY_SIZE),
+            _phantom: PhantomData
         }
     }
 }
 
-impl BloomFilter<T> {
+impl<T: Hash + ?Sized> BloomFilter<T> {
     // define a new function
     pub fn new() -> Self {
         Self::default()
     }
 
-    // difine with_capacity function
-    pub fn with_capacity(capacity: usize) -> Self {
-        let hash_init_vec = vec![Box::new(RandomState::new())];
-        let hash_array = HasherArray::from(hash_init_vec);
-        BloomFilter {
-            capacity: capacity,
-            hashers: hash_array,
-            bit_array: BitVec::repeat(false, capacity)
+    // define with_capacity function
+    // pub fn with_capacity(capacity: usize) -> Self {
+    //     let hash_init_vec = vec![Box::new(RandomState::new())];
+    //     let hash_array = HasherArray::from(hash_init_vec);
+    //     BloomFilter {
+    //         capacity,
+    //         hashers: hash_array,
+    //         bit_array: BitVec::repeat(false, capacity),
+    //         _phantom: PhantomData
+    //     }
+    // }
+
+    // define a function: calculate the hash value, get the mod value
+    fn calculate_hash(&self, idx: usize, item: &T) -> u64 {
+        let mut hasher = self.hashers[idx].build_hasher();
+        item.hash(&mut hasher);
+        hasher.finish() % (self.capacity as u64)
+    }
+
+    // define a function: set bit status
+    pub fn set_bit(&mut self, item: &T) {
+        for idx in 0..self.hashers.len() {
+            let bit_hash_value = self.calculate_hash(idx, item);
+            self.bit_array.set(bit_hash_value as usize, true);
         }
+    }
+
+    // define a function: get bit status
+    pub fn judge_contain(&self, item: &T) -> bool {
+        for idx in 0..self.hashers.len() {
+            let bit_hash_value = self.calculate_hash(idx, item);
+            match self.bit_array.get(bit_hash_value as usize) {
+                Some(result) => {
+                    if !result { return false; } // if bit offset is false, return false
+                },
+                _ => return false // if get None, return false
+            }
+        }
+        true
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,51 @@
+use bitvec::macros::internal::funty::Fundamental;
+
 mod bf;
-use bitvec::prelude::*;
 
 fn main() {
     // println!("Hello, world!");
-    let test_array = bitarr![u32, Lsb0; 0; 80];
-    println!("test_array: {:?}", test_array);
+    let mut bf = bf::BloomFilter::new();
+
+    let test_continuous_nums = 0..10000;
+    let positive_nums = 5000..10000;
+    let negative_nums = 10000..15000;
+
+    let test_continuous_count = test_continuous_nums.clone().count();
+    let positive_count = positive_nums.clone().count();
+    let negative_count = negative_nums.clone().count();
+
+
+    // set bit in bloom filter of 10000 continuous numbers
+    println!("Start to set bit in bloom filter of {} continuous numbers", test_continuous_count);
+    for i in test_continuous_nums {
+        bf.set_bit(&i.to_string());
+    }
+
+    println!("Start to judge if {} positive numbers are in bloom filter", positive_count);
+    let mut true_positive = 0;
+    for i in positive_nums {
+        if bf.judge_contain(&i.to_string()) {
+            true_positive += 1;
+        }
+    }
+    println!("true-positive-rate: {}/{} = {}", true_positive, positive_count, true_positive.as_f64()/ positive_count.as_f64());
+
+
+    println!("Start to judge if {} negative numbers are in bloom filter", negative_count);
+    let mut false_positive = 0;
+    for i in negative_nums {
+        if bf.judge_contain(&i.to_string()) {
+            false_positive += 1;
+        }
+    }
+    println!("false-positive-rate: {}/{} = {}", false_positive, negative_count, false_positive.as_f64()/ negative_count.as_f64());
+
+
+    // FP rate = (1-(1-1/m)^nk)^k
+
+    let m = 10240f64; // bit array size
+    let n = test_continuous_count.as_f64(); // insert number
+    let k = 1f64; // hasher number
+    let fp_rate = (1.0 - (1.0 - 1.0/m).powf(n as f64 * k as f64)).powf(k as f64);
+    println!("Theoretical false-positive-rate: {}", fp_rate);
 }
